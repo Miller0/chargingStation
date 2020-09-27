50b2b75be27f1051b89ce36ddf054f9751aade7b
Miller0 <mr.milller2017.11@gmail.com>
2020-09-27 17:47:25 +0300

    *

--- modules/api/Module.php
+++ modules/api/Module.php
@@ -37,7 +37,7 @@ class Module extends \yii\base\Module
             'rules' => [
                 [
                     'allow' => true,
-                    'roles' => ['@'],
+                    'roles' => ['?'],
                 ],
             ],
         ];
